b6d1fc810c706998f82b69b53be984d1562725a2
Sairaghav Gubba <gubba.sairaghav@gmail.com>
2026-04-30 04:57:22 -0700

    Set embedding_openrouter.py to version from main branch to resolve merge conflicts.

--- code/backend/models/embedding_openrouter.py
+++ code/backend/models/embedding_openrouter.py
@@ -1,11 +1,15 @@
 """A Wrapper for the langchain class to be instantiated by the model factory."""
 
+from __future__ import annotations
+
 import os
-from typing import override
+from typing import TYPE_CHECKING, override
 
 from backend.models._embedding_base import _BaseEmbedding
-from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
+
+if TYPE_CHECKING:
+    from langchain_core.documents import Document
 
 
 class EmbeddingOpenRouter(_BaseEmbedding):
@@ -13,19 +17,27 @@ class EmbeddingOpenRouter(_BaseEmbedding):
 
     _BASE_URL = "https://openrouter.ai/api/v1"
 
-    def __init__(self, kwargs: dict) -> None:
-        """Create an instance of the EmbeddingOpenRouter."""
-        model_name: str = kwargs.pop("model")
+    def __init__(self, model_name: str | dict, **kwargs) -> None:
+        """Create an instance of the ChatbotOpenRouter."""
+        if isinstance(model_name, dict):
+            config = dict(model_name)
+            model_name = str(config.pop("model"))
+            config.pop("temperature", None)
+            kwargs = {**config, **kwargs}
+
         self._model = OpenAIEmbeddings(
             model=model_name,
             api_key=os.getenv("OPENROUTER_API_KEY"),  # type: ignore (can't get SecretStr type)
             base_url=self._BASE_URL,
             check_embedding_ctx_length=False,
+            # dimensions=self._VECTOR_DIMENSIONS,
             **kwargs,
         )
 
     @override
     def embed_document(self, document: Document) -> tuple[list[float], Document]:
+        # note: we only pass str content when embedding, but we store
+        # metadata with the vector in the db
         return self._model.embed_query(document.page_content)[: self._VECTOR_DIMENSIONS], document
 
     @override
