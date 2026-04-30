5cb94b28b6a8b44fc0b62bc2fab1d873471643bb
Sairaghav Gubba <gubba.sairaghav@gmail.com>
2026-04-30 04:54:03 -0700

    Set embedding_openrouter.py to version from main branch to resolve merge conflicts.

--- code/backend/models/embedding_openrouter.py
+++ code/backend/models/embedding_openrouter.py
@@ -16,7 +16,6 @@ class EmbeddingOpenRouter(_BaseEmbedding):
     def __init__(self, kwargs: dict) -> None:
         """Create an instance of the EmbeddingOpenRouter."""
         model_name: str = kwargs.pop("model")
-        # base_url: str = kwargs.pop("base_url", self._BASE_URL)
         self._model = OpenAIEmbeddings(
             model=model_name,
             api_key=os.getenv("OPENROUTER_API_KEY"),  # type: ignore (can't get SecretStr type)
