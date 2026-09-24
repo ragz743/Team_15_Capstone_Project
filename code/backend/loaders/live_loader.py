850e99236ee3145e33c47bba75a3d04d672119db
Zuriel Hernandez <zurielahg@outlook.com>
2026-09-24 12:13:18 -0700

    Preserve weather record dates (#60)

--- code/backend/loaders/live_loader.py
+++ code/backend/loaders/live_loader.py
@@ -42,10 +42,10 @@ class LiveQueryResult(NamedTuple):
 
     @classmethod
     def from_dict(cls, d: dict[str, object]) -> Self:
-        """Create a DailyQueryResult from a tuple."""
+        """Create a live observation from database fields."""
         return cls(
             # lots of type ignores, types based on MySQL - annoying but ok for now!
-            d["TSTAMP"].strftime("%Y-%m-%d"),  # type: ignore
+            d["TSTAMP"].strftime("%Y-%m-%d %H:%M:%S"),  # type: ignore
             d.get("AIR_TEMP"),  # type: ignore
             d.get("REL_HUMIDITY"),  # type: ignore
             d.get("PRECIP"),  # type: ignore
